d6cd5299a97cf7483cd6046d17e73607306e6830
Stalkie666 <hamatlama@gmail.com>
2022-02-17 16:48:20 +0100

    Zakladni ovladani - nedokonceno

--- Snake_CSharp/Form1.Designer.cs
+++ Snake_CSharp/Form1.Designer.cs
@@ -30,12 +30,96 @@ namespace Snake_CSharp
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnSnap = new System.Windows.Forms.Button();
+            this.picCanvas = new System.Windows.Forms.PictureBox();
+            this.txtScore = new System.Windows.Forms.Label();
+            this.txtHightScore = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.picCanvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(864, 55);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(92, 60);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStartGame);
+            // 
+            // btnSnap
+            // 
+            this.btnSnap.Location = new System.Drawing.Point(864, 173);
+            this.btnSnap.Name = "btnSnap";
+            this.btnSnap.Size = new System.Drawing.Size(92, 60);
+            this.btnSnap.TabIndex = 1;
+            this.btnSnap.Text = "Snap";
+            this.btnSnap.UseVisualStyleBackColor = true;
+            this.btnSnap.Click += new System.EventHandler(this.takeSnapShot);
+            // 
+            // picCanvas
+            // 
+            this.picCanvas.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.picCanvas.Location = new System.Drawing.Point(46, 55);
+            this.picCanvas.Name = "picCanvas";
+            this.picCanvas.Size = new System.Drawing.Size(764, 764);
+            this.picCanvas.TabIndex = 2;
+            this.picCanvas.TabStop = false;
+            this.picCanvas.Paint += new System.Windows.Forms.PaintEventHandler(this.UpdatePictureBoxGraphic);
+            // 
+            // txtScore
+            // 
+            this.txtScore.AutoSize = true;
+            this.txtScore.Location = new System.Drawing.Point(864, 296);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(48, 15);
+            this.txtScore.TabIndex = 3;
+            this.txtScore.Text = "Score: 0";
+            // 
+            // txtHightScore
+            // 
+            this.txtHightScore.AutoSize = true;
+            this.txtHightScore.Location = new System.Drawing.Point(864, 337);
+            this.txtHightScore.Name = "txtHightScore";
+            this.txtHightScore.Size = new System.Drawing.Size(81, 15);
+            this.txtHightScore.TabIndex = 4;
+            this.txtHightScore.Text = "Hight Score: 0";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 40;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1094, 877);
+            this.Controls.Add(this.txtHightScore);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.picCanvas);
+            this.Controls.Add(this.btnSnap);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.picCanvas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnSnap;
+        private System.Windows.Forms.PictureBox picCanvas;
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.Label txtHightScore;
+        private System.Windows.Forms.Timer gameTimer;
     }
 }
 
--- Snake_CSharp/Form1.cs
+++ Snake_CSharp/Form1.cs
@@ -12,10 +12,91 @@ namespace Snake_CSharp
 {
     public partial class Form1 : Form
     {
+        private List<Circle> Snake = new List<Circle>();
+        private Circle food = new Circle();
+
+        int maxWidth;
+        int maxHeight;
+
+        int score;
+        int hightScore;
+
+        Random rand = new Random();
+
+        bool goLeft, goRight, goDown, goUp;
+
+
         public Form1()
         {
             InitializeComponent();
+
+            new Settings();
         }
 
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left && Settings.direction != "right")
+            {
+                goLeft = true;
+            }
+            else if(e.KeyCode == Keys.Right && Settings.direction != "left")
+            {
+                goRight = true;
+            }
+            else if(e.KeyCode == Keys.Up && Settings.direction != "down")
+            {
+                goUp = true;
+            }
+            else if(e.KeyCode == Keys.Down && Settings.direction != "up")
+            {
+                goDown = true;
+            }
+            else
+            {
+                //do nothing
+            }
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void btnStartGame(object sender, EventArgs e)
+        {
+
+        }
+
+        private void takeSnapShot(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gameTimerEvent(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UpdatePictureBoxGraphic(object sender, PaintEventArgs e)
+        {
+
+        }
+
+
+        //custom functions
+        private void RestartGame()
+        {
+
+        }
+
+        private void EatFood()
+        {
+
+        }
+
+        private void GameOver()
+        {
+
+        }
     }
 }
